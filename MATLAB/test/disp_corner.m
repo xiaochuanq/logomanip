9085d0b5798bf76a348f78bd05a5a7de9ce5d0a6
Xiaochuan <xqin@seas.upenn.edu>
2010-12-18 00:41:32 -0500

    Directory update

--- MATLAB/test/disp_corner.m
+++
@@ -1,6 +0,0 @@
-function disp_corner(I, Y, X, color)
-I = uint8(I);
-imshow(I);
-hold on;
-scatter(X, Y, 'o', color);
-hold off;
--- MATLAB/test/disp_inlier_match.m
+++
@@ -1,7 +0,0 @@
-function disp_inlier_match(y1, x1, y2, x2, im1rgb, im2rgb, inlier_ind)
-X1 = x1(inlier_ind);
-Y1 = y1(inlier_ind);
-X2 = x2(inlier_ind);
-Y2 = y2(inlier_ind);
-subplot(1,2,1); disp_corner(im1rgb, Y1, X1); 
-subplot(1,2,2); disp_corner(im2rgb, Y2, X2);
